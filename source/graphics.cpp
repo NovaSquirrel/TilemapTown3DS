2a7c4d083a87b98e29b90668b6dccd41c79f0415
NovaSquirrel <bushytail@novasquirrel.com>
2023-02-17 23:22:45 -0500

    Swizzle textures with the CPU instead

--- source/graphics.cpp
+++ source/graphics.cpp
@@ -43,7 +43,17 @@ static u32 next_power_of_two(u32 v) {
 	return v + 1;
 }
 
-int texture_loaded_yet = 0;
+// LUT from http://problemkaputt.de/gbatek-3ds-video-texture-swizzling.htm
+static u8 swizzle_lut[64] = {
+	0x00, 0x01, 0x08, 0x09, 0x02, 0x03, 0x0A, 0x0B,
+	0x10, 0x11, 0x18, 0x19, 0x12, 0x13, 0x1A, 0x1B,
+	0x04, 0x05, 0x0C, 0x0D, 0x06, 0x07, 0x0E, 0x0F,
+	0x14, 0x15, 0x1C, 0x1D, 0x16, 0x17, 0x1E, 0x1F,
+	0x20, 0x21, 0x28, 0x29, 0x22, 0x23, 0x2A, 0x2B,
+	0x30, 0x31, 0x38, 0x39, 0x32, 0x33, 0x3A, 0x3B,
+	0x24, 0x25, 0x2C, 0x2D, 0x26, 0x27, 0x2E, 0x2F,
+	0x34, 0x35, 0x3C, 0x3D, 0x36, 0x37, 0x3E, 0x3F,
+};
 
 void http_png_callback(const char *url, uint8_t *memory, size_t size, TilemapTownClient *client, void *userdata) {
 	printf("Url %s Size %d\n", url, size);
@@ -52,7 +62,7 @@ void http_png_callback(const char *url, uint8_t *memory, size_t size, TilemapTow
 	// Based on https://github.com/asiekierka/atari800-3ds/blob/ee116ce923ccaafd76e2c44d51c71fe969f07ea2/src/3ds/grapefruit.c
 
 	png_image image;
-	u32 *data;
+	u32 *linear_pixels, *swizzled_pixels;
 	C3D_Tex* tex = (C3D_Tex*)linearAlloc(sizeof(C3D_Tex));
 
 	memset(&image, 0, sizeof(image));
@@ -62,27 +72,52 @@ void http_png_callback(const char *url, uint8_t *memory, size_t size, TilemapTow
 
 	image.format = PNG_FORMAT_ABGR;
 
-	C3D_TexInitVRAM(tex, next_power_of_two(image.width), next_power_of_two(image.height), GPU_RGBA8);
-	data = (u32*)linearAlloc(tex->width * tex->height * sizeof(u32));
+	// Prepare a place to put the decoded PNG, and decode it
 
-	if(!png_image_finish_read(&image, NULL, data, tex->width * sizeof(u32), NULL)) {
-		linearFree(data);
+	C3D_TexInit(tex, next_power_of_two(image.width), next_power_of_two(image.height), GPU_RGBA8);
+	linear_pixels   = (u32*)linearAlloc(tex->width * tex->height * sizeof(u32));
+	swizzled_pixels = (u32*)linearAlloc(tex->width * tex->height * sizeof(u32));
+	size_t stride = tex->width * sizeof(u32);
+
+	if(!png_image_finish_read(&image, NULL, linear_pixels, stride, NULL)) {
+		linearFree(linear_pixels);
+		linearFree(swizzled_pixels);
 		C3D_TexDelete(tex);
 		return;
 	}
 
-	GSPGPU_FlushDataCache(data, tex->width * tex->height * sizeof(u32));
+	// Swizzle the texture
+	memset(swizzled_pixels, 0xff, tex->width * tex->height * sizeof(u32));
 
-	C3D_SyncDisplayTransfer(data, GX_BUFFER_DIM(tex->width, tex->height),
-		(u32*)tex->data, GX_BUFFER_DIM(tex->width, tex->height),
-		GX_TRANSFER_FLIP_VERT(1) | GX_TRANSFER_OUT_TILED(1) | GX_TRANSFER_RAW_COPY(0) |
-		GX_TRANSFER_IN_FORMAT(GPU_RGBA8) | GX_TRANSFER_OUT_FORMAT(GPU_RGBA8)
-		| GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO));
+	u32 *sw = swizzled_pixels;
+	for(size_t ty = 0; ty < tex->height/8; ty++) {
+		for(size_t tx = 0; tx < tex->width/8; tx++) {
+			for(size_t px =0; px<64; px++) {
+				u8 from_table = swizzle_lut[px];
+				u8 table_x = from_table & 7;
+				u8 table_y = (from_table >> 3) & 7;
 
-	linearFree(data);
+				*sw = linear_pixels[(tex->height-1-(ty*8+table_y))*(tex->width) + (tx*8+table_x)];
+				sw++;
+			}
+		}
+	}
+
+	C3D_TexUpload(tex, swizzled_pixels);
+	C3D_TexSetFilter(tex, GPU_LINEAR, GPU_NEAREST);
+	C3D_TexSetWrap(tex, GPU_REPEAT, GPU_REPEAT);
+	C3D_TexBind(0, tex);
+
+	linearFree(linear_pixels);
+	linearFree(swizzled_pixels);
 
 	client->texture_for_url[std::string(url)] = tex;
 	client->need_redraw = true;
+	puts("Finished decoding texture");
+}
+
+bool string_is_http_url(std::string &url) {
+	return url.starts_with("https://") || url.starts_with("http://");
 }
 
 C2D_Image* Pic::get(TilemapTownClient *client) {
@@ -92,13 +127,12 @@ C2D_Image* Pic::get(TilemapTownClient *client) {
 
 	// Try to turn a key into a URL if needed
 	std::string *real_url = &this->key;
-	if(!this->key.starts_with("https://") && !this->key.starts_with("http://")) {
+	if(!string_is_http_url(this->key)) {
 		auto it2 = client->url_for_tile_sheet.find(this->key);
 		if(it2 != client->url_for_tile_sheet.end()) {
 			real_url = &(*it2).second;
 		} else {
 			if(client->requested_tile_sheets.find(this->key) != client->requested_tile_sheets.end()) {
-				puts("Requesting a tileset");
 				client->requested_tile_sheets.insert(this->key);
 				return nullptr;
 			}
@@ -123,6 +157,7 @@ C2D_Image* Pic::get(TilemapTownClient *client) {
 void TilemapTownClient::draw_map(int camera_x, int camera_y) {
 	if(!this->map_received)
 		return;
+	this->animation_tick = (this->animation_tick+1) % 60000;
 
 	for(int y=0; y<15; y++) {
 		for(int x=0; x<25; x++) {
@@ -153,6 +188,50 @@ void TilemapTownClient::draw_map(int camera_x, int camera_y) {
 					C2D_DrawImageAt(*image, x*16, y*16, 0, NULL, 1.0f, -1.0f);
 				}
 			}
+		}
+	}
+
+	// Draw entities
+
+	for(auto& [key, entity] : this->who) {
+		if(entity.walk_timer)
+			entity.walk_timer--;
+
+		const C2D_Image *image = entity.pic.get(this);
+		if(image) {
+			int tileset_width  = image->tex->width;
+			int tileset_height = image->tex->height;
+			bool player_is_16x16 = false;
+
+			if(tileset_width == 16 && tileset_height == 16) {
+				player_is_16x16 = true;
+
+				Tex3DS_SubTexture subtexture = calc_subtexture(image->tex->width, image->tex->height, 16, 16, 0, 0);
+				C2D_Image new_image = {image->tex, &subtexture};
+				C2D_DrawImageAt(new_image, (entity.x*16)-camera_x, (entity.y*16)-camera_y, 0, NULL, 1.0f, -1.0f);
+			} else if(string_is_http_url(entity.pic.key)) {
+				int frame_x = 0, frame_y = 0;
+				int frame_count_from_animation_tick = this->animation_tick / 6;
+				bool is_walking = entity.walk_timer != 0;
+
+				switch(tileset_height / 32) { // Directions
+					case 2: frame_y = entity.direction_lr / 4; break;
+					case 4: frame_y = entity.direction_4 / 2; break;
+					case 8: frame_y = entity.direction; break;
+				}
+				switch(tileset_width / 32) { // Frames per direction
+					case 2: frame_x = (is_walking * 1); break;
+					case 4: frame_x = (is_walking * 2) + (frame_count_from_animation_tick & 1); break;
+					case 8: frame_x = (is_walking * 4) + (frame_count_from_animation_tick & 3); break;
+				}
+
+				Tex3DS_SubTexture subtexture = calc_subtexture(image->tex->width, image->tex->height, 32, 32, frame_x, frame_y);
+				C2D_Image new_image = {image->tex, &subtexture};
+				C2D_DrawImageAt(new_image, (entity.x*16-8)-camera_x, (entity.y*16-16)-camera_y, 0, NULL, 1.0f, -1.0f);
+			} else {
+				C2D_DrawImageAt(*image, (entity.x*16)-camera_x, (entity.y*16)-camera_y, 0, NULL, 1.0f, -1.0f);
+			}
+
 		}
 	}
 }
